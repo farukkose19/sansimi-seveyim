2f187a6fbb84b860791e38a343e9f1e3e51e6370
faruk köse <>
2022-06-26 00:57:08 +0300

    firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvUajU9B5kNt_x2SJ1kHUWhKchwLa_Tps",
+    authDomain: "sansimi-seveyim.firebaseapp.com",
+    projectId: "sansimi-seveyim",
+    storageBucket: "sansimi-seveyim.appspot.com",
+    messagingSenderId: "652471742427",
+    appId: "1:652471742427:web:bef540bce89b71e569c1fe",
+    measurementId: "G-5BKQQRYHT2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvUajU9B5kNt_x2SJ1kHUWhKchwLa_Tps",
+    authDomain: "sansimi-seveyim.firebaseapp.com",
+    projectId: "sansimi-seveyim",
+    storageBucket: "sansimi-seveyim.appspot.com",
+    messagingSenderId: "652471742427",
+    appId: "1:652471742427:web:bef540bce89b71e569c1fe",
+    measurementId: "G-5BKQQRYHT2"
+  }
 };
 
 /*
